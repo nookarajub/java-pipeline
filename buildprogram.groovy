807cd71281b5bb6cd3406d4c3e3cc2eea705f767
nookarajub <71743311+nookarajub@users.noreply.github.com>
2021-01-30 15:32:19 -0600

    Update buildprogram.groovy

--- buildprogram.groovy
+++ buildprogram.groovy
@@ -2,6 +2,10 @@ def buildMyCode(def url, def branch)
 {
 //git url: 'https://github.com/nookarajub/demo-test', credentialsId: 'nookaraju-jenkins-new', branch : 'master'
   git credentialsId: 'nookaraju-jenkins-new', url: "${url}", branch: "${branch}"
+  
+  tools {
+        maven 'maven_software' 
+    }
 
 bat "mvn clean install"
 
